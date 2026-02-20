7272f4527644bdac55e35fec3bfe2809a00882d4
jzaletelj <janez.zaletelj@gmail.com>
2026-02-20 17:27:10 +0100

    Testiranje JupyterLab

---
+++ jupyter_server_config.py
@@ -0,0 +1,8 @@
+c.AiExtension.blocked_providers = [
+    'ai21', 'bedrock', 'bedrock-chat', 'bedrock-custom', 'anthropic-chat',
+    'azure-chat-openai', 'cohere', 'gemini', 'gpt4all', 'huggingface_hub',
+    'mistralai', 'nvidia-chat', 'openai', 'openai-chat', 'openai-chat-custom',
+    'openrouter', 'qianfan', 'sagemaker-endpoint', 'togetherai', 'vertexai'
+]
+# OR for allowlist:
+# c.AiExtension.allowed_providers = ['ollama']
